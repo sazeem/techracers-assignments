0284d2549af49f9017f3cfd392f5a22a099c0bd7
Syed <sazeem@grepruby.com>
2018-07-19 12:04:18 +0530

    Deleted Trash

--- July10/Assignment1/myJS2.js
+++
@@ -1,4 +0,0 @@
-var unflatObject=  {"flatJSON": false, "i": { "am": { "not": { "so": { "flat": true, "unflat": false } }, "a": "tree" } }, "dates": [ { "day": 1 }, { "day": 8947 } ] };
-//output =  {"flatJSON": false, "i.am.not.so.flat": true, "i.am.not.so.unflat": false, "i.am.a": "tree", "dates.0.day": 1, "dates.1.day": 8947 }
-
-
