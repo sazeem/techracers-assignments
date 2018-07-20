04c8faddbdeaf4d9d83ca3f7fcdb607b4ff87513
Syed <sazeem@grepruby.com>
2018-07-20 21:42:07 +0530

    Adding new files

--- July20/classNameTest.js
+++ July20/classNameTest.js
@@ -1,35 +1,34 @@
-ar chai = require('chai');
+var chai = require('chai');
 var assert = chai.assert;
-
-var className = require('../js/className.js');
+var className = require('./className');
 var addClass = className.addClass;
 
 // The rest of the file remains the same
 
 describe('addClass', function() {
+
   it('should add class to element', function() {
+    
     var element = { className: '' };
-
     addClass(element, 'test-class');
-
     assert.equal(element.className, 'test-class');
   });
 
-  it('should not add a class which already exists'){
-  var element = { className: 'exists' };
+  it('should not add a class which already exists',function(){
 
-  addClass(element, 'exists');
+    var element = { className: 'exists' };
+    addClass(element, 'exists');
+    var numClasses = element.className.split(' ').length;
+    assert.equal(numClasses, 1);
+  
+  });
+  
+  it('should append new class after existing one', function() {
 
-  var numClasses = element.className.split(' ').length;
-  assert.equal(numClasses, 1);
-	};
-	it('should append new class after existing one', function() {
-  var element = { className: 'exists' };
-
-  addClass(element, 'new-class');
-
-  var classes = element.className.split(' ');
-  assert.equal(classes[1], 'new-class');
-});
+    var element = { className: 'exists' };
+    addClass(element, 'new-class');
+    var classes = element.className.split(' ');
+    assert.equal(classes[1], 'new-class');
+  });
 
 });
